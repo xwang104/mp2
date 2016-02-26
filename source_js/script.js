3e31f40f456ad40a1ae71107a5c8722815f7d7b9
Xiaoxiao Wang <xiaoxiaowang@Xiaoxiaos-MacBook-Pro.local>
2016-02-26 01:43:43 -0600

    nav sticky and responsive, part0 content and resize

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,22 @@
 // Write any custom javascript functions here
+  $('#nonresponsive-topbar').on('sticky.zf.stuckto:top', function(){
+  $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function(){
+  $(this).removeClass('shrink');
+})
+
+var part0 = $('#part0'),
+    imgWidth = 1200,
+    imgHeight = 800,
+    imgRatio = imgHeight/imgWidth,
+    part0Height = part0.height(),
+    part0Width = part0.width();
+
+$(document).ready(function() {
+    part0.height(imgRatio*part0Width);
+});
+
+$(window).resize(function() {
+    part0Width = part0.width();
+	part0.height(imgRatio*part0Width);
+});
