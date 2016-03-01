a62631eee49e1ca44cf6542a2e66b7e4245b8e10
Xiaoxiao Wang <xiaoxiaowang@Xiaoxiaos-MacBook-Pro.local>
2016-03-01 16:50:45 -0600

    mobile menu hide after click, change some fonts

--- source_js/script.js
+++ source_js/script.js
@@ -3,13 +3,17 @@
 
 //-----------------------navigation bar sticky and resize-------------------
 
-  $('#nonresponsive-topbar').on('sticky.zf.stuckto:top', function(){
-  $(this).addClass('shrink');
-  $('#title').show();
+    $('#nonresponsive-topbar').on('sticky.zf.stuckto:top', function(){
+    $(this).addClass('shrink');
+    $('#title').show();
 }).on('sticky.zf.unstuckfrom:top', function(){
-  $(this).removeClass('shrink');
-  $('#title').hide();
+    $(this).removeClass('shrink');
+    $('#title').hide();
 })
+    $('#responsive-topbar').find('a').click(function() {
+    	$('#responsive-topbar').delay(500).fadeOut();
+    })
+
 
 //-----------------------part0 resize with background image-------------------
 
